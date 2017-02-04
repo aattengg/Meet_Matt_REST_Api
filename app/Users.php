93e180b37f78ac7153f45ec2651ba34e16b253e3
aattengg <anthony.tenggoro@gmail.com>
2017-02-04 00:52:16 -0500

    Fixed silly migration issues. Migration seemed to work for users table, but not interactions table

--- app/Users.php
+++ app/Users.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Users extends Model
 {
-    //
+    // Define 'interactions' relationships for users
+    public function userInteraction() {
+        return $this->hasMany('App/Interactions','user_id'); 
+    }
 }
